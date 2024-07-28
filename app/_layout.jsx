3e6d51caf613362027a7204471c3b69c568a0842
Souverain joseph <158466435+panther-souverain@users.noreply.github.com>
2024-07-29 01:12:50 +0200

    custom Tabs nave

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import TabBar from "../components/TabBar";
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs tabBar={(props) => <TabBar {...props} />}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
-      {/* <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="index" options={{ title: "Home" }} /> */}
+      <Tabs.Screen name="explore" options={{ title: "Explore" }} />
+      <Tabs.Screen name="create" options={{ title: "Create" }} />
+      <Tabs.Screen name="profil" options={{ title: "Profil" }} />
     </Tabs>
   );
 };
