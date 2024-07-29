ab897b7d5f42fb1755fbd302903b3905f8150e53
Souverain joseph <158466435+panther-souverain@users.noreply.github.com>
2024-07-30 00:35:55 +0200

    je voulais ajouter des data

--- constents/Data.js
+++ constents/Data.js
@@ -3,50 +3,90 @@ export const Data = [
     Id: "03f04915-cc6f-4668-b9c8-42ec8399a847",
     Name: "Lela Rosenbaum",
     Photo: require("../assets/imageData/image1.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "6d2ff6f1-9d95-46c5-9e89-207a34b31918",
     Name: "Saul Murphy",
     Photo: require("../assets/imageData/image2.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "2b8d338e-3016-405c-a323-4628cb4f9aee",
     Name: "Joy Hintz",
     Photo: require("../assets/imageData/image3.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "9727ed5a-9e33-411b-889a-4f65145aeccf",
     Name: "Ken Reichert",
     Photo: require("../assets/imageData/image4.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "23dc056a-7a8a-4adf-aeb5-3db8a46ef63a",
     Name: "Elena Carter",
     Photo: require("../assets/imageData/image5.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "447711ff-8ad5-42a7-9f9f-e36f4e884e76",
     Name: "Eileen Weissnat",
     Photo: require("../assets/imageData/image6.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "68b3820e-4ca4-429d-804c-9b375bb9b97f",
     Name: "Sara Huel",
     Photo: require("../assets/imageData/image7.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "5eb89734-6924-4784-be26-519c7a04c69e",
     Name: "Stella Lesch",
     Photo: require("../assets/imageData/image8.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "ea4e2621-798f-43a0-b6c9-bc36403f5f7e",
     Name: "Milton O'Conner",
     Photo: require("../assets/imageData/image9.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
   {
     Id: "54d769c8-5116-45ce-ae52-d36275dbda96",
     Name: "Sean Klein",
     Photo: require("../assets/imageData/image10.png"),
+    price: 99.99,
+    selected: false,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, consequatur dolor sequi provident possimus sed ad qui ipsum similique odio iste molestias praesentium nemo ut facere, consectetur error in voluptate!",
   },
 ];
