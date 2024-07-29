c531dc258e8292cc3b76fb271f4b798f42804a4c
Souverain joseph <158466435+panther-souverain@users.noreply.github.com>
2024-07-29 14:46:54 +0200

    custom index

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Entypo, Ionicons } from "@expo/vector-icons";
+import ImageSlider from "../components/imageSlider";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <View style={styles.containerHeader}>
+        <Entypo name="menu" size={30} color="white" />
+        <Text style={styles.titleHeader}>BINGO SHOP</Text>
+        <Ionicons name="notifications-outline" size={30} color="white" />
+      </View>
+      <View style={styles.containerSearch}>
+        <Text style={styles.searchTitle}>Search ...</Text>
+      </View>
+      <ImageSlider />
     </View>
   );
 };
@@ -14,8 +24,34 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: "#040404",
+    paddingTop: 30,
+  },
+  containerHeader: {
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  titleHeader: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "white",
+  },
+  containerSearch: {
+    alignItems: "flex-start",
+    justifyContent: "center",
+    backgroundColor: "#262627",
+    paddingHorizontal: 20,
+    paddingVertical: 13,
+    margin: 15,
+    borderRadius: 15,
+  },
+  searchTitle: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#555252",
   },
 });
---
+++ components/imageSlider.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ImageSlider = () => {
+  return (
+    <View>
+      <Text>ImageSlider</Text>
+    </View>
+  );
+};
+
+export default ImageSlider;
+
+const styles = StyleSheet.create({});
